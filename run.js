9855e3fa5e1f2016571059b125fc38c8b19efa6c
Rodrigo Alfonso <rodri042@gmail.com>
2014-12-22 18:59:33 -0300

    Agrego más información en el logueo

--- run.js
+++ run.js
@@ -1,17 +1,21 @@
 var request = require("request");
 
-var syncUrl = "https://parsimotion-syncer.azurewebsites.net/hooks/webjob/notification"
+var syncUrl = "https://parsimotion-syncer.azurewebsites.net/api/hooks/webjob";
+//syncUrl = "http://localhost:9000/api/hooks/webjob";
+
 var token = "*****";
 
 var options = {
     url: syncUrl,
     metod: "POST",
     headers: {
-        "Signature": token
+        "signature": token
     }
 };
 
 request.post(options, function(err, coso) {
-	if (err) { console.err(new Date() + ": Falla"); }
-	else { console.log(new Date() + ": OK"); }
+	if (err)
+		console.error(new Date() + ": Falla...\n" + err);
+	else
+		console.log(new Date() + ": OK");
 });
