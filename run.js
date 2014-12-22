47c7acb32f0a4aabc2106ac37d69447ea1e3517b
Rodrigo Alfonso <rodri042@gmail.com>
2014-12-22 19:26:55 -0300

    Ahora anda y el log muestra el resultado de la sincronización

--- run.js
+++ run.js
@@ -1,21 +1,23 @@
 var request = require("request");
 
 var syncUrl = "https://parsimotion-syncer.azurewebsites.net/api/hooks/webjob";
-//syncUrl = "http://localhost:9000/api/hooks/webjob";
-
+syncUrl = "http://localhost:9000/api/hooks/webjob";
 var token = "*****";
+
+var userId = "*****";
 
 var options = {
     url: syncUrl,
-    metod: "POST",
-    headers: {
-        "signature": token
-    }
+    headers: { signature: token },
+	body: { userId: userId },
+	json: true
 };
 
-request.post(options, function(err, coso) {
+request.post(options, function(err, result) {
 	if (err)
 		console.error(new Date() + ": Falla...\n" + err);
-	else
-		console.log(new Date() + ": OK");
+	else {
+		result = result.body;
+		console.log(new Date() + ": OK (fulfilled: %d, failed: %d, unlinked: %d)", result.fulfilled.length, result.failed.length, result.failed.length);
+	}
 });
